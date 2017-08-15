542b1d66b25fda89d98b31b1b835ebcf9d04ef81
Tomasz Bakula <tbakula92@gmail.com>
2017-08-15 19:42:40 +0100

    Refactor App component.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyCpq9koxa25i8PVXi3KTog7GFw4ZJ2JmOo';
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar/>
-        </div>
-    );
+class App extends Component {
+
+    constructor (props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({ key: API_KEY, term: 'summer' }, videos => {
+            this.setState({ videos });
+        });
+    }
+
+    render () {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
+
 }
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
