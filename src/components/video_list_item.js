58649df3c413e567284be9fbdeea2d09f7ee49b7
Tomasz Bakula <tbakula92@gmail.com>
2017-08-15 20:08:42 +0100

    Display data in VideoListItem component.

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
-    return <li>Video</li>;
+const VideoListItem = ({video}) => {
+    const imageURL = video.snippet.thumbnails.default.url;
+    const title = video.snippet.title;
+
+    return (
+        <li className="list-group-item">
+            <div className="video-list media">
+
+                <div className="media-left">
+                    <img className="media-object" src={imageURL} />
+                </div>
+
+                <div className="media-body">
+                    <div className="media-heading">{title}</div>
+                </div>
+
+            </div>
+        </li>
+    );
 };
 
 export default VideoListItem;
