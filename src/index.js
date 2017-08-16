ab082e03ff5caf070c0b32ba177449804935fd0b
Tomasz Bakula <tbakula92@gmail.com>
2017-08-16 20:23:28 +0100

    Display VideoDetail component.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyCpq9koxa25i8PVXi3KTog7GFw4ZJ2JmOo';
 
@@ -22,6 +23,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoDetail video={this.state.videos[0]} />
                 <VideoList videos={this.state.videos} />
             </div>
         );
