c5d17436dc75bce8b3c76b7ce46d40280a974557
Tomasz Bakula <tbakula92@gmail.com>
2017-08-18 17:31:53 +0100

    Display video details.

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -10,16 +10,16 @@ const VideoDetail = ({video}) => {
 
     return (
         <div className="video-detail col-md-8">
+
             <div className="embed-responsive embed-responsive-16by9">
-
                 <iframe className="embed-responsive-item" src={url}></iframe>
-
-                <div className="details">
-                    <div>{video.snippet.title}</div>
-                    <div>{video.snippet.description}</div>
-                </div>
-
             </div>
+
+            <div className="details">
+                <div>{video.snippet.title}</div>
+                <div>{video.snippet.description}</div>
+            </div>
+
         </div>
     );
 };
