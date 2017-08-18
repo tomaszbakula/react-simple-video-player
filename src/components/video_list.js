d7ce1e5bb285a53d1c7d9b936d69aac9fa0722ef
Tomasz Bakula <tbakula92@gmail.com>
2017-08-18 17:18:48 +0100

    Make the video list clickable.

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import VideoListItem from './video_list_item';
 
-const VideoList = (props) => {
-    const videoItems = props.videos.map((video) => {
-        return <VideoListItem key={video.etag} video={video} />;
+const VideoList = ({videos, onVideoSelect}) => {
+    const videoItems = videos.map((video) => {
+        return (
+            <VideoListItem
+                onVideoSelect={onVideoSelect}
+                key={video.etag}
+                video={video} />
+        );
     });
 
     return (
--- src/index.js
+++ src/index.js
@@ -12,10 +12,16 @@ class App extends Component {
     constructor (props) {
         super(props);
 
-        this.state = { videos: [] };
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
 
         YTSearch({ key: API_KEY, term: 'summer' }, videos => {
-            this.setState({ videos });
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
         });
     }
 
@@ -23,8 +29,10 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
-                <VideoDetail video={this.state.videos[0]} />
-                <VideoList videos={this.state.videos} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
             </div>
         );
     }
